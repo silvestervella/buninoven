815e28743a358bcd18090f980e4c569ffb8266a4
Silvester Vella <silvester@Silvesters-iMac.lan>
2020-03-12 17:25:09 +0100

    dfgbf

--- theme_files/functions.php
+++ theme_files/functions.php
@@ -7,14 +7,14 @@ function rb_child_theme_setup() {
 
 
 // Load HTML5 Blank Child styles
-function wearandrevel_styles_child()
+function buninoven_styles_child()
 {
     // Register Child Styles
-    wp_register_style('wearandrevel-child', get_stylesheet_directory_uri() . '/style.css', array(), '1.0', 'all');
+    wp_register_style('buninoven-child', get_stylesheet_directory_uri() . '/style.css', array(), '1.0', 'all');
     wp_register_style('child-all', get_stylesheet_directory_uri() . '/css/all.css', array(), '1.0', 'all');
 
     // Enqueue Child Styles
-    wp_enqueue_style('wearandrevel-child'); 
+    wp_enqueue_style('buninoven-child'); 
     wp_enqueue_style('child-all');
 
     //Register Child Scripts
@@ -26,7 +26,7 @@ function wearandrevel_styles_child()
     wp_enqueue_script( 'theme-script' );   
 
 }
-add_action('wp_enqueue_scripts', 'wearandrevel_styles_child', 20); // Add Theme Child Stylesheet
+add_action('wp_enqueue_scripts', 'buninoven_styles_child', 20); // Add Theme Child Stylesheet
 
 
 function load_admin_style() {
